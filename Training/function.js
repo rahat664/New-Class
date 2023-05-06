7c5e75bc5c27347f3c5ace29557c8a390251c0d9
rahat664 <rahatkabir20@gmail.com>
2023-05-07 01:52:15 +0600

    committed all files

---
+++ Training/function.js
@@ -0,0 +1,27 @@
+// function add(a, b) {
+//     return a + b;
+// }
+//
+// function subtraction(a, b) {
+//     return a - b;
+// }
+//
+// function multiply(a, b) {
+//     return a * b;
+// }
+//
+// // console.log(add(7,9))
+// // console.log(subtraction(7,9))
+// // console.log(multiply(7,9))
+//
+// let addition = add(34, 42)
+// let subs = subtraction(34, 42)
+// let multi = multiply(34, 42)
+//
+// console.log(`Addition of a and b is ${add(40, 560)} \nSubtraction of a and b is ${subs} \nmultiplication of a and b is ${multi}
+// `)
+
+//Arrow function
+let add = (a,b) => {return a+b}
+
+console.log(add(7,9))
---
+++ Training/statement.js
@@ -0,0 +1,13 @@
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+console.log(c);
+
+let name = 'rahat'
+let age = 25
+let university = 'Dhaka International University'
+
+console.log('my name is '+ name + ' & my age is ' + age + ' & i read in ' + university)
+
+console.log(`my name is ${name} & my age is ${age} & i read in ${university}`)
